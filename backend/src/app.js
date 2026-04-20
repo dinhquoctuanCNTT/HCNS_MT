ebe0d43c073a25203d38caabbd73dec2e61992d1
Tuan Dinh <dinhtuna30@gmail.com>
2026-04-20 15:06:55 +0700

    Fix CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,17 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "ngrok-skip-browser-warning",
+    ],
+  }),
+);
 app.use(express.json());
 
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
